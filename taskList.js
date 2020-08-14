869b9c63231449c667193cdd42723faa1fe9707b
Akhat-cpp <56387863+Akhat-cpp@users.noreply.github.com>
2020-08-15 02:05:25 +0300

    Add files via upload

--- taskList.js
+++ taskList.js
@@ -1,12 +1,17 @@
 tasksArray = {};
 delTasksArray = {};
 counter = 0;
-function addTask(id, txt)
+function addTask(id, txt, idInp, impButton)
 {
+	if(txt == "" || txt == " ")
+		return;
 	let idRow = id + counter;
 	tasksArray[id + "." + counter + ".text"] = txt;
-	tasksArray[id + "." + counter + ".elemHTML"] = '<p class="task" id="' + idRow + '">' + txt +'<span class="taskBtDel" onclick="deleteRow(`' + idRow + '`,`' + counter + '`)">Del</span></p>';
+	tasksArray[id + "." + counter + ".elemHTML"] = '<div class="task" id="' + idRow + '"><p class="taskTxt">' + txt +'</p><p class="taskBtDel" onclick="deleteRow(`' + idRow + '`,`' + counter + '`)">Del</p></div>';
+	// if(impButton)
+	// 		tasksArray[id + "." + counter + ".elemHTML"] = '<div class="taskImp" id="' + idRow + '"><p class="taskTxt">' + txt +'</p><p class="taskBtDel" onclick="deleteRow(`' + idRow + '`,`' + counter + '`)">Del</p></div>';
 	document.getElementById(id).innerHTML += tasksArray[id + "." + counter + ".elemHTML"];
+	document.getElementById(idInp).value = "";
 	counter++;
 }
 function deleteRow(id, counter)
@@ -15,4 +20,14 @@ function deleteRow(id, counter)
 	delete tasksArray[id + "." + counter + ".text"];
 	delete tasksArray[id + "." + counter + ".elemHTML"];
 	document.getElementById(id).remove();
+}
+function resetInputAndAddTask(idInp)
+{
+	addTask("tasks", document.getElementById(idInp).value, idInp);
+	document.getElementById(idInp).value = "";
+}
+function key(event)
+{
+	if(event.code === "Enter")
+		resetInputAndAddTask(event.path[0].attributes[0].nodeValue);
 }
